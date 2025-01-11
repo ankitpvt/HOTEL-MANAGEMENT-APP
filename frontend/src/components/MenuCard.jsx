7dfef2139bae84cecc526089a0c49e9d06dd8da1
Ankit Patil <ankitpatil9307@gmail.com>
2025-01-11 15:58:16 +0530

    local

--- frontend/src/components/MenuCard.jsx
+++ frontend/src/components/MenuCard.jsx
@@ -8,8 +8,8 @@ const MenuCard = () => {
   
     useEffect(() => {
       const fetchMenu = async () => {   //https://hotel-management-app-backend.vercel.app/
-        const response = await axios.get("https://backend-vercel-3slxhsd3m-ankitpvts-projects.vercel.app/api/menu");
-        setMenu(response.data);        //https://backend-vercel-3slxhsd3m-ankitpvts-projects.vercel.app
+        const response = await axios.get("http://localhost:5000/api/menu");
+        setMenu(response.data);
       };
       fetchMenu();
     }, []);
@@ -26,7 +26,7 @@ const MenuCard = () => {
     //   addOrder(order);
   
       // Optionally, send the order to the backend
-      const response = await axios.post("https://backend-vercel-3slxhsd3m-ankitpvts-projects.vercel.app/api/orders", order);
+      const response = await axios.post("http://localhost:5000/api/orders", order);
       alert(`Order placed successfully! Order ID: ${response.data._id}`);
     };
   
